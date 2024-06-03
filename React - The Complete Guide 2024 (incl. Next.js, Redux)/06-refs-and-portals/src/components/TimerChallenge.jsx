95aaec660831d0db4741acf87dd38fbec19f45bb
drengr <kasprzyk.daniel4@gmail.com>
2024-06-03 23:34:20 +0200

    Added 06 refs and portals

---
+++ React - The Complete Guide 2024 (incl. Next.js, Redux)/06-refs-and-portals/src/components/TimerChallenge.jsx
@@ -0,0 +1,59 @@
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
+
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleStartChallenge() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
+  }
+
+  function handleStopChallenge() {
+    dialog.current.open();
+    clearInterval(timer.current);
+  }
+
+  function handleResetChallenge() {
+    console.log("Resetting challenge");
+    setTimeRemaining(targetTime * 1000);
+  }
+
+  return (
+    <>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleResetChallenge}
+      />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button
+            onClick={timerIsActive ? handleStopChallenge : handleStartChallenge}
+          >
+            {timerIsActive ? "Stop " : "Start "}Challenge
+          </button>
+        </p>
+        <p className={timerIsActive ? "active" : ""}>
+          {timerIsActive ? "Timer is running" : "Timer inactive"}
+        </p>
+      </section>
+    </>
+  );
+}
