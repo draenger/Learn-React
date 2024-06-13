7d451b5d1d7ed71d0512cb077f5900ed08edbb6e
drengr <kasprzyk.daniel4@gmail.com>
2024-06-13 22:49:09 +0200

    added react optimalisation

---
+++ React - The Complete Guide 2024 (incl. Next.js, Redux)/11-react-optimalization/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import MillionLint from "@million/lint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    MillionLint.vite(), //does not work
+
+    react(),
+  ],
+});
